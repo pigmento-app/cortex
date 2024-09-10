39273e026201ee26d67adac42c327385b19c0dc6
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2024-09-10 15:59:53 +0200

    feat(home): Placed the elements in the correct layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, Button, View, Text } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedView } from '@/components/ThemedView';
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedView } from "@/components/ThemedView";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Camera from "@/assets/svg/camera.svg";
+import { Fonts } from "@/constants/Fonts";
+import { Ellipse, Svg } from "react-native-svg";
 
 export default function HomeScreen() {
   const [image, setImage] = useState<string | null>(null);
-  const [colorCounts, setColorCounts] = useState<{ [key: string]: number } | null>(null);
+  const [colorCounts, setColorCounts] = useState<{
+    [key: string]: number;
+  } | null>(null);
+  const [score, setScore] = useState<number>(0);
 
   const takePhoto = async () => {
     const { status } = await ImagePicker.requestCameraPermissionsAsync();
-    if (status !== 'granted') {
-      alert('Permission to access camera is required!');
+    if (status !== "granted") {
+      alert("Permission to access camera is required!");
       return;
     }
 
@@ -23,80 +38,130 @@ export default function HomeScreen() {
     if (!result.canceled) {
       const uri = result.assets[0].uri;
       setImage(uri);
-      uploadImage(uri);  // Appel à la fonction d'upload
+      uploadImage(uri); // Appel à la fonction d'upload
     }
   };
 
   const uploadImage = async (uri: string) => {
     const formData = new FormData();
-    formData.append('file', {
+    formData.append("file", {
       uri,
-      type: 'image/jpeg',
-      name: 'photo.jpg',
+      type: "image/jpeg",
+      name: "photo.jpg",
     });
 
-    console.log(formData)
+    console.log(formData);
 
     try {
-      const response = await fetch('https://votre-api.com/upload', {
-        method: 'POST',
+      const response = await fetch("https://votre-api.com/upload", {
+        method: "POST",
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
         body: formData,
       });
       const result = await response.json();
-      console.log('Upload success', result);
+      console.log("Upload success", result);
     } catch (error) {
-      console.error('Upload failed', error);
+      console.error("Upload failed", error);
     }
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <Button title="Take Photo" onPress={takePhoto} />
-        {image && <Image source={{ uri: image }} style={styles.image} />}
-        {colorCounts && (
-          <View>
-            {Object.entries(colorCounts).map(([color, count]) => (
-              <Text key={color}>
-                {color}: {count} pixels
-              </Text>
-            ))}
+    <SafeAreaView style={styles.safeAreaView}>
+      <ThemedView style={styles.mainContainer}>
+        <View>
+          <Text style={[Fonts.title, styles.mainTitle]}>Pigmento.</Text>
+          <View style={styles.infoContainer}>
+            <View
+              style={{ flexDirection: "row", gap: 10, alignItems: "center" }}
+            >
+              <Svg height="40" width="40">
+                {/* Set the cx, cy to 20 and rx, ry to 20 for a 40px circle */}
+                <Ellipse cx="20" cy="20" rx="20" ry="20" fill="red" />
+              </Svg>
+              <Text style={Fonts.info}>Couleur du jour</Text>
+            </View>
+            <Text style={Fonts.info}>0 points</Text>
           </View>
-        )}
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity onPress={takePhoto} style={styles.takePhotoBtn}>
+            <Camera width={24} height={24} />
+            <Text style={styles.takePhotoBtnText}>Prendre une photo</Text>
+          </TouchableOpacity>
+
+          {image && <Image source={{ uri: image }} style={styles.image} />}
+
+          {/* {colorCounts && (
+            <View style={styles.colorCountsContainer}>
+              {Object.entries(colorCounts).map(([color, count]) => (
+                <Text key={color} style={styles.colorCountText}>
+                  {color}: {count} pixels
+                </Text>
+              ))}
+            </View>
+          )} */}
+        </View>
       </ThemedView>
-    </ParallaxScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-    justifyContent: 'center',
-    marginTop: 20,
+  safeAreaView: {
+    backgroundColor: "#111",
+    flex: 1,
+    padding: 0,
+  },
+  mainContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#111",
+    paddingHorizontal: 20,
+  },
+  mainTitle: {
+    alignSelf: "center",
+  },
+  infoContainer: {
+    marginTop: 40,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  btnContainer: {
+    backgroundColor: "#111",
+    marginVertical: 20,
+    flex: 1, // Takes the available vertical space
+    justifyContent: "center",
+  },
+  takePhotoBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    backgroundColor: "white",
+    marginBottom: 20,
+  },
+  takePhotoBtnText: {
+    color: "#111",
+    fontSize: 16,
   },
   image: {
     width: 200,
     height: 200,
     marginTop: 10,
+    borderRadius: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  colorCountsContainer: {
+    marginTop: 20,
+  },
+  colorCountText: {
+    color: "#000",
+    fontSize: 14,
   },
 });
---
+++ constants/Fonts.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const Fonts = StyleSheet.create({
+  title: {
+    fontFamily: "Inter Tight",
+    fontSize: 30,
+    fontWeight: "700", // Numeric value or use 'bold'
+    color: "white",
+  },
+  info: {
+    fontFamily: "Inter Tight",
+    fontSize: 18,
+    fontWeight: "400", // Numeric value or use 'normal'
+    color: "white",
+  },
+});
