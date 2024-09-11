f05dcd54223cde344c7eb964e749de6b7fcb75bf
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2024-09-11 12:30:59 +0200

    feat(light-dark-theme): Made it so the app background and text color change according to phone theme

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -8,6 +8,7 @@ import {
   ScrollView,
   TouchableOpacity,
   Platform,
+  useColorScheme,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
@@ -16,7 +17,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Camera from "@/assets/svg/camera.svg";
 import { Fonts } from "@/constants/Fonts";
 import { Ellipse, Svg } from "react-native-svg";
-import fs from "fs";
+import { Colors } from "@/constants/Colors";
 
 export default function HomeScreen() {
   const [image, setImage] = useState<string | null>(null);
@@ -24,6 +25,7 @@ export default function HomeScreen() {
     [key: string]: number;
   } | null>(null);
   const [score, setScore] = useState<number>(0);
+  const theme = useColorScheme() ?? "light";
 
   const takePhoto = async () => {
     const { status } = await ImagePicker.requestCameraPermissionsAsync();
@@ -79,49 +81,60 @@ export default function HomeScreen() {
   };
 
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <ThemedView style={styles.mainContainer}>
-        <View>
-          <Text style={[Fonts.title, styles.mainTitle]}>Pigmento.</Text>
-          <View style={styles.infoContainer}>
-            <View
-              style={{ flexDirection: "row", gap: 10, alignItems: "center" }}
-            >
-              <Svg height="40" width="40">
-                {/* Set the cx, cy to 20 and rx, ry to 20 for a 40px circle */}
-                <Ellipse cx="20" cy="20" rx="20" ry="20" fill="red" />
-              </Svg>
-              <Text style={Fonts.info}>Couleur du jour</Text>
-            </View>
-            <Text style={Fonts.info}>{score} points</Text>
+    <View
+      style={[
+        styles.safeAreaView,
+        { backgroundColor: Colors[theme].background },
+      ]}
+    >
+      <ThemedView
+        style={[
+          styles.mainContainer,
+          { backgroundColor: Colors[theme].background },
+        ]}
+      >
+        <View style={styles.infoContainer}>
+          <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
+            <Svg height="40" width="40">
+              {/* Set the cx, cy to 20 and rx, ry to 20 for a 40px circle */}
+              <Ellipse cx="20" cy="20" rx="20" ry="20" fill="red" />
+            </Svg>
+            <Text style={[Fonts.info, { color: Colors[theme].text }]}>
+              Couleur du jour
+            </Text>
           </View>
+          <Text style={[Fonts.info, { color: Colors[theme].text }]}>
+            {score} points
+          </Text>
         </View>
-        <View style={styles.btnContainer}>
-          <TouchableOpacity onPress={takePhoto} style={styles.takePhotoBtn}>
+        <View
+          style={[
+            styles.btnContainer,
+            { backgroundColor: Colors[theme].background },
+          ]}
+        >
+          <TouchableOpacity
+            onPress={takePhoto}
+            style={[
+              styles.takePhotoBtn,
+              {
+                borderWidth: 2,
+                borderColor: theme === "dark" ? "white" : "black",
+              },
+            ]}
+          >
             <Camera width={24} height={24} />
             <Text style={styles.takePhotoBtnText}>Prendre une photo</Text>
           </TouchableOpacity>
-
           {image && <Image source={{ uri: image }} style={styles.image} />}
-
-          {/* {colorCounts && (
-            <View style={styles.colorCountsContainer}>
-              {Object.entries(colorCounts).map(([color, count]) => (
-                <Text key={color} style={styles.colorCountText}>
-                  {color}: {count} pixels
-                </Text>
-              ))}
-            </View>
-          )} */}
         </View>
       </ThemedView>
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   safeAreaView: {
-    backgroundColor: "#111",
     flex: 1,
     padding: 0,
   },
@@ -129,11 +142,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#111",
     paddingHorizontal: 20,
-  },
-  mainTitle: {
-    alignSelf: "center",
   },
   infoContainer: {
     marginTop: 40,
@@ -143,7 +152,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   btnContainer: {
-    backgroundColor: "#111",
     marginVertical: 20,
     flex: 1, // Takes the available vertical space
     justifyContent: "center",
