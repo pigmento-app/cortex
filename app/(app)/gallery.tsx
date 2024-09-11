8de6fdaffead46e358e740bf22ee5a83b4f11e34
DFarau <doriane.farau@hetic.net>
2024-09-11 20:44:13 +0200

    feat(app): add gallery

--- app/(app)/gallery.tsx
+++ app/(app)/gallery.tsx
@@ -1,11 +1,103 @@
-import { useSession } from "@/context/authContext";
-import { Text } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Image, FlatList, ActivityIndicator, useColorScheme } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "@/constants/Colors";
 
-export default function Gallery(){
-    const { session: token } = useSession();
+const IMAGE = "https://via.placeholder.com/150";
 
-    console.log('Token:', token);
-    return(
-        <Text>Gallery</Text>
-    )
+export default function Gallery() {
+  const colorScheme = useColorScheme();
+
+  const [images, setImages] = useState<(string | null)[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        // requête API
+        // const response = await fetch("https://api");
+        // const data = await response.json();
+        // setImages(data.images);
+
+        // simulation
+        const totalImages = 50;
+        const allImages = Array.from({ length: totalImages }, (_, i) => `${IMAGE}?img=${i + 1}`);
+        setImages(allImages);
+      } catch (error) {
+        console.error("Erreur lors du chargement des images :", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchImages();
+  }, []);
+
+  const numColumns = 3;
+  const fullRows = Math.floor(images.length / numColumns);
+  let numberElementsLastRow = images.length - (fullRows * numColumns);
+
+  if (numberElementsLastRow !== 0) {
+    while (numberElementsLastRow !== numColumns) {
+      images.push(null);
+      numberElementsLastRow++;
+    }
+  }
+
+  const renderImage = ({ item }: { item: string | null }) => {
+    return (
+      <View style={styles.imageContainer}>
+        {item ? (
+          <Image
+            source={{ uri: item }}
+            style={styles.image}
+          />
+        ) : (
+          null        
+        )}
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView  style={[styles.safeAreaView, { backgroundColor: Colors[colorScheme ?? "light"].background }]}>
+      <FlatList
+        data={images}
+        renderItem={renderImage}
+        keyExtractor={(item, index) => item ? item : `empty-${index}`}
+        numColumns={numColumns}
+        contentContainerStyle={styles.grid}
+        ListFooterComponent={
+          loading ? (
+            <View style={styles.spinnerContainer}>
+              <ActivityIndicator size="small" />
+            </View>
+          ) : null
+        }
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  grid: {
+    paddingHorizontal: 2,
+  },
+  imageContainer: {
+    flex: 1,
+    margin: 2,
+    aspectRatio: 1,
+    backgroundColor: 'transparent',
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 5,
+    resizeMode: 'cover',
+  },
+  spinnerContainer: {
+    marginVertical: 20,
+  },
+});
