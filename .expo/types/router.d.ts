9b1809efd66b71d7bd81de83c63d0abd0ad74054
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-09-12 15:56:55 +0200

    merge main

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(app)` | `/(app)/` | `/(app)/gallery` | `/(app)/settings` | `/..\components\common\ArrowButton` | `/_sitemap` | `/auth/signIn` | `/auth/signUp` | `/gallery` | `/onboarding` | `/settings`;
+      StaticRoutes: `/` | `/(app)` | `/(app)/` | `/(app)/gallery` | `/(app)/settings` | `/_sitemap` | `/auth/signIn` | `/auth/signUp` | `/gallery` | `/onboarding` | `/settings`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
